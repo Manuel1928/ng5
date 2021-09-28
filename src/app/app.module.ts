e22a38fa6f53e94255c3c7b8dcc18561dd6ce38b
Manuel1928 <manuel1998angel@gmail.com>
2021-09-28 12:06:01 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { DataService } from './data.service';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
