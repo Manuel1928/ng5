4cac60e32014d0e90b5471e92624f6c02f1dcecb
Manuel1928 <manuel1998angel@gmail.com>
2021-09-28 09:12:01 -0500

    update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  xit(`should have as title 'ng5'`, () => {
+  it(`should have as title 'ng5'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ng5');
